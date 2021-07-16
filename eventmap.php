b225c095e20f5b3109e9487c4992ead5e3e78214
Jan <jan.lorenz@live.de>
2021-07-16 18:12:35 +0200

    added first database support

--- eventmap.php
+++ eventmap.php
@@ -5,10 +5,45 @@
  * Description:       A map with event markers
  * Author:            @fuzzelmukkel, @SirRick
  * License:           MIT
- * Na
  */
 
+ /*
+  * Functions
+  */
 
+  /*
+   * DB Handler
+   */
+
+
+  function db_create_tables() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . "eventmap_data";
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+      id int NOT NULL AUTO_INCREMENT,
+      title varchar(50) not null,
+      lon float not null,
+      lat float not null,
+      PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+  }
+
+  function db_delete_tables() {
+    global $wpdb;
+      $table_name = $wpdb->prefix . 'eventmap_data';
+      $sql = "DROP TABLE IF EXISTS $table_name";
+      $wpdb->query($sql);
+  }
+
+  /*
+   * REST API
+   */
 
  function rest_endpoint( $data ) {
 
@@ -16,6 +51,9 @@
  }
 
 
+/*
+ * SHORTCODE
+ */
 
  function eventmap_shortcode( $atts = [], $content = null, $tag = '' ) {
 
@@ -23,7 +61,7 @@
  }
 
  /**
-  * Central location to create all shortcodes.
+  * Initializers
   */
  function eventmap_shortcodes_init() {
      add_shortcode( 'eventmap', 'eventmap_shortcode' );
@@ -54,9 +92,32 @@
    );
  }
 
+/*
+ * Plugin management
+ */
+
+ function activate_eventmap() {
+   db_create_tables();
+ }
+
+ function deactivate_eventmap() {
+   db_delete_tables();
+
+ }
+
+ function uninstall_eventmap() {
+ }
+
+/*
+ * ADD HOOKS
+ */
+
  add_action('init', 'eventmap_shortcodes_init');
  add_action('rest_api_init', 'eventmap_rest_init');
  add_action('admin_menu', 'eventmap_add_options_page');
 
+ register_activation_hook(__FILE__, 'activate_eventmap');
+ register_deactivation_hook(__FILE__, 'deactivate_eventmap');
+ register_uninstall_hook(__FILE__, 'uninstall_eventmap');
 
 ?>
