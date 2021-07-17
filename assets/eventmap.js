dad9031243fe963fb21fbd8439f0a1ccded566c4
Jan <jan.lorenz@live.de>
2021-07-17 19:39:51 +0200

    MVP 1 finished

--- assets/eventmap.js
+++ assets/eventmap.js
@@ -1,8 +1,60 @@
-jQuery(function(){
 
-    var map = L.map('eventmap').setView([51.505, -0.09], 13);
+var map = null
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  	}).addTo(map);
-});
+function calculateMid(data) {
+  lat = 0
+  lon = 0;
+
+  data.forEach((item, i) => {
+    lat += parseFloat(item['lat'])
+    lon += parseFloat(item['lon'])
+  });
+
+  i = data.length
+  return [lat/i, lon/i]
+}
+
+function setMarkers(data) {
+  data.forEach((item, i) => {
+      latlon = [parseFloat(item['lat']), parseFloat(item['lon'])]
+
+      marker = L.marker(latlon)
+                .addTo(map)
+                .bindPopup(item['title']);
+  })
+}
+
+function processData(data) {
+  // calculate center
+  console.log(data)
+
+  mid = calculateMid(data)
+  map.setView(mid, 10)
+
+  setMarkers(data)
+}
+
+function requestData() {
+  jQuery.ajax("?rest_route=/eventmap/v1/events")
+    .done(function(data){processData(data)})
+    .error(function(){console.log('eventmap: error loading data')})
+}
+
+function init() {
+
+  //initialize map
+  map = L.map('eventmap');
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  map.setView([51.505, -0.09], 13)
+
+
+  requestData();
+}
+
+
+// run init when page has loaded
+jQuery(init);
---
+++ dummydata.sql
@@ -0,0 +1,4 @@
+insert into wp_eventmap_data(title, lat, lon) values ('test1', 52.546295697522886, 13.353538513183596);
+insert into wp_eventmap_data(title, lat, lon) values ('test2', 52.50305664701985, 13.404006958007814);
+insert into wp_eventmap_data(title, lat, lon) values ('bärenfutter', 52.51392263399709, 13.43559265136719);
+insert into wp_eventmap_data(title, lat, lon) values ('eene deemo', 52.51099744023003, 13.3154296875);
